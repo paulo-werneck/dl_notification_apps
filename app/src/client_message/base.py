e2916ee7c2636d4fb7c73c2e98ee33baea63498f
Paulo Werneck <werneck.suporte@gmail.com>
2021-08-31 16:44:56 -0300

    added abc client message and empty modules client message

---
+++ app/src/client_message/base.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+
+class ClientMessageBase(ABC):
+
+    def __init__(self, payload_sns):
+        self.payload_sns = payload_sns
+
+    def __parse_message(self):
+        return self.payload_sns.get("Records")[0].get("Sns")
+
+    @property
+    def subject(self):
+        return self.__parse_message().get("Subject")
+
+    @property
+    def message(self):
+        return self.__parse_message().get("Message")
+
+    @abstractmethod
+    def push_client_notification(self):
+        pass
